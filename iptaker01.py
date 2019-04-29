b435164bfb3c496f64ad04214e2a79d2b3f92829
Brian Foster <bpfoster7@hotmail.com>
2019-04-29 14:27:03 -0400

    v1

---
+++ iptaker01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+user_input = input("Please enter and IPv4 IP address:")
+
+print("You told me the IPV4 address is:" + user_input)
+
+print("\n")
+
+vend_input = input("Please enter the Vendor name:")
+print("You told me the Vendor is: " + vend_input)
